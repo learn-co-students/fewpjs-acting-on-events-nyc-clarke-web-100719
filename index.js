15e57deb38ae0ad6a862f3885b2c8fa520c922e6
gnardinosaur <github email address>
2019-11-20 09:19:33 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 5}px`;
+    }
+}
+
+function moveDodgerRight() {
+    let rightNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(rightNumbers);
+
+    dodger.style.left = `${left + 5}px`; 
+}
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+})
