e138ea4d39824468dd3cd1b9c6a1f0345f143743
TJBender <tiana.bender01@gmail.com>
2019-11-25 03:57:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+// SOME VARIABLES AND CONTROLS!!
+let dodger = document.getElementById("dodger");
+let leftNumbers = dodger.style.left.replace("px", "");
+let left = parseInt(leftNumbers, 10);
+
+// LISTEN FOR KEYS 
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+    else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
+// MAKE IT MOVEEE!!! 
+function moveDodgerLeft() {
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
